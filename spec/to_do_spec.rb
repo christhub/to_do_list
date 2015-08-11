c0b27a63a56bf33db2adac6cbf1e5e28b39fa870
Andrew Haupt & Christian Lei <student@epicodus.com>
2015-08-11 09:17:44 -0700

    Write code and spec initializing variables and taking a description

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('to_do')
+
+describe (Task) {
+  describe ('#description') {
+    it("lets you give it a description of a task") {
+      test_task = Task.new("make me coffee")
+      expect(test_task.description).to(eq("make me coffee"))
+    }
+  }
+}
