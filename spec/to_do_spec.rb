cb9b6cbe58577e60c265bc73963106c2d8014a4a
Andrew Haupt & Christian Lei <student@epicodus.com>
2015-08-11 09:34:51 -0700

    Write code for clearing the class array

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -8,17 +8,25 @@ describe (Task) {
       expect(test_task.description).to(eq("make me coffee"))
     }
   }
-}
 
-describe ('.all') {
-  it ("is empty at first") {
-    expect(Task.all).to(eq([]))
-  }
-}
-describe ('#save') {
-  it ("adds a task to the array of saved tasks") {
-      new_task = Task.new("water the cactus")
-      new_task.save()
-      expect(Task.all()).to(eq([new_task]))
+  describe ('.all') {
+    it ("is empty at first") {
+      expect(Task.all).to(eq([]))
     }
+  }
+  describe ('#save') {
+    it ("adds a task to the array of saved tasks") {
+        new_task = Task.new("water the cactus")
+        new_task.save()
+        expect(Task.all()).to(eq([new_task]))
+      }
+  }
+  describe('.clear') {
+    it ("clears the list of tasks that were saved") {
+      new_task = Task.new("feed the midget")
+      new_task.save
+      Task.clear
+      expect(Task.all).to(eq([]))
+    }
+  }
 }
